54de917ea690b28ca3d0ef902008a5f203fc5543
spelll <itmikno@gmail.com>
2024-09-01 20:02:59 +0900

    remove xcopy commands from backup-clean scripts

--- scripts/win-build-skip.bat
+++ scripts/win-build-skip.bat
@@ -11,9 +11,9 @@ if errorlevel 1 (
 cd ..
 
 echo 'Cleaning/Backup Builds'
-xcopy \y app\build app1\build
-xcopy \y app\kernel app1\kernel
-xcopy \y app\kernel-arm64 app1\kernel-arm64
+@REM xcopy /e app\build builds\build
+@REM xcopy /e app\kernel builds\kernel
+@REM xcopy /e app\kernel-arm64 builds\kernel-arm64
 del /S /Q /F app\build 1>nul
 del /S /Q /F app\kernel 1>nul
 del /S /Q /F app\kernel-arm64 1>nul
